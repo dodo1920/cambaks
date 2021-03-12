faa826b59fc92fc565bab30c5782ec353991b03a
do <ssonagi032@gmail.com>
2021-03-12 20:22:53 +0900

    도연 / homeController 수정

--- src/main/java/com/cambak21/controller/HomeController.java
+++ src/main/java/com/cambak21/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.cambak21.controller;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -18,13 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cambak21.domain.CheckListVO;
+import com.cambak21.service.checkList.CheckListServiceImpl;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-<<<<<<< HEAD
    
    @Inject
    private CheckListServiceImpl CheckListService;
@@ -104,79 +105,6 @@ public class HomeController {
       }   
       return entity;
    }
-=======
-	
-	@Inject
-	private com.cambak21.service.checkList.CheckListService CheckListService;
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-
-	@RequestMapping(value = "/testcambak21", method = RequestMethod.GET)
-	public String cambak21home(Locale locale, Model model) {
-		logger.info("백승권 / 캠박이일 홈페이지 테스트 진입");
-	
-		
-		return "cambakMain/cambak21Main";
-	}
-
-		
-	@RequestMapping(value = "/mall", method = RequestMethod.GET)
-	public String homeheader(Locale locale, Model model) {
-		logger.info("JJONG homeheader 작업중 . . . . .");
-	
-		return "/cambakMall/mall";
-	}
-	
-	
-	@RequestMapping(value = "/boardNotice", method = RequestMethod.GET)
-	public String boardNotice(Locale locale, Model model) {
-		logger.info("JJONG boardNotice 작업중 . . . . .");
-			
-		return "boardNotice";
-	}
-	
-	@RequestMapping(value="/checkList", method = RequestMethod.GET)
-	public String checkList() {
-		logger.info("JJONG ckeckList2 작업중 . . . . .");
-	
-		
-		return "checkList2";
-	}
-	
-	@RequestMapping(value="/ajaxCheckList/{member_id}", method=RequestMethod.GET)
-	public ResponseEntity<List<CheckListVO>> lst(@PathVariable("member_id") String member_id){
-		// bno에 대한 전체 글 보기
-		
-		ResponseEntity<List<CheckListVO>> entity = null;
-		try {
-			entity = new ResponseEntity<List<CheckListVO>>(CheckListService.getCheckList(member_id), HttpStatus.OK);
-			System.out.println(entity.toString());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST); // 예외가 발생하면 List<ReplyVO> 는 null 이므로 ...ResponseEntity<>
-		}   
-		return entity;
-	}
->>>>>>> a6579fbb01937f37c555548783a88cb3ba53f515
 
 }
-
 	
