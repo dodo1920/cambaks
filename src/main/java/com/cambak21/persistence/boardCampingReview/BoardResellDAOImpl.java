fba5cca73f226fc5b5d3a281b0bc2cf18623d635
ktehun1234 <ktehun1234@gmail.com>
2021-03-12 15:50:55 +0900

    BoardDAO까지완

---
+++ src/main/java/com/cambak21/persistence/boardCampingReview/BoardResellDAOImpl.java
@@ -0,0 +1,39 @@
+package com.cambak21.persistence.boardCampingReview;
+
+import java.util.List;
+
+import com.cambak21.domain.ResellBoardVO;
+
+public class BoardResellDAOImpl implements BoardResellDAO {
+
+	@Override
+	public int ResellBoardInsert(ResellBoardVO vo) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<ResellBoardVO> ResellBoardReadAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResellBoardVO ResellBoardRead(int no) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int ResellBoardUpdate(ResellBoardVO vo, int no) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ResellBoardDelete(ResellBoardVO vo, int no) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
