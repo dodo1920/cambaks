dd889ddbc7be1d41c34a114a66687a886fd361cf
DESKTOP-LEQNT4V\goott6 <bbsk3939@gmail.com>
2021-03-15 19:19:41 +0900

    승권 / replyBoardCsMapper.xml 작성 완료, BoardCsController 수정중

--- src/main/java/com/cambak21/controller/boards/BoardCsController.java
+++ src/main/java/com/cambak21/controller/boards/BoardCsController.java
@@ -2,20 +2,27 @@ package com.cambak21.controller.boards;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cambak21.service.boardCS.BoardCsService;
 
-@Controller("/board/cs/*")
+@Controller
+@RequestMapping("/board/cs")
 public class BoardCsController {
 	
 	@Inject
 	private BoardCsService service;
 	
+	private static Logger logger = LoggerFactory.getLogger(BoardCsController.class);
+	
 	@RequestMapping("/")
 	public String BoardCsList() {
-		return null;
+		logger.info("승권 / BoardCsController 호출");
+		
+		return "cambakMain/cambak21Board";
 	}
 	
 }
