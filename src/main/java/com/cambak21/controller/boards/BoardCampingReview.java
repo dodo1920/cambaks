d5a7b19e5b59fd5802196a145431032d222b4171
glory <beauglory@gmail.com>
2021-03-19 18:36:37 +0900

    이영광 / campingreview list 수정

--- src/main/java/com/cambak21/controller/boards/BoardCampingReview.java
+++ src/main/java/com/cambak21/controller/boards/BoardCampingReview.java
@@ -4,30 +4,36 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cambak21.controller.DAOTest;
 import com.cambak21.domain.BoardVO;
+import com.cambak21.service.boardCS.BoardCsService;
 import com.cambak21.service.boardCampingReview.CampingReviewService;
 import com.cambak21.service.boardProdReview.ProdReviewService;
+import com.cambak21.util.PagingCriteria;
 
 @Controller
-@RequestMapping(value="/board/*")
+@RequestMapping(value="/board/campingreview")
 public class BoardCampingReview {
 
 	@Inject
 	private CampingReviewService service;
 	
-	@RequestMapping(value="/camping-reviewAll", method = RequestMethod.GET)
-	public String campingreview(Model model) throws Exception{
-//		logger.info("/listAll.... get 호출");
+	private static final Logger logger = LoggerFactory.getLogger(BoardCampingReview.class);
 
-		List<BoardVO> lst = service.campingReviewAll();
-		model.addAttribute("boardList", lst);
+		
+	@RequestMapping("/")
+	public String BoardCsList(Model model, PagingCriteria cri) throws Exception {
+		logger.info("영광 / 게시글 리스트 get방식 호출");
+		
+		model.addAttribute("boardList", service.listCampingReviewCriteria(cri));
+		
 		return "cambakMain/board/campingreview/camping-review";
 	}
 	
