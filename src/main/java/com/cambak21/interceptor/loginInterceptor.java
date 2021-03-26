b292ed0d701bbc3733d7beafcf952656914203f1
do <ssonagi032@gmail.com>
2021-03-26 17:47:41 +0900

    도연 / find_idPwd 유저 이메일 체크 + login interceptor 수정

--- src/main/java/com/cambak21/interceptor/loginInterceptor.java
+++ src/main/java/com/cambak21/interceptor/loginInterceptor.java
@@ -15,63 +15,64 @@ import com.cambak21.domain.MemberVO;
 
 public class loginInterceptor extends HandlerInterceptorAdapter{
 
-	
-	private static Logger logger = LoggerFactory.getLogger(loginInterceptor.class);
-	
-//	@Override
-//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//			throws Exception {
-//		
-//		logger.info("로그인 하기 전 . . . . . . . . . .");
-//		HttpSession ses = request.getSession();
-//		
-//		if(ses.getAttribute("loginMember") != null) {    // 이전 로그인 정보가 세션에 남아 있다면
-//				ses.removeAttribute("loginMember");   // 로그인 정보 삭제
-//		}
-//		
-//		return true;	// 이후 인터셉터 또는 컨트롤러에게 제어권을 넘김
-//	}
-//	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
+   
+   private static Logger logger = LoggerFactory.getLogger(loginInterceptor.class);
+   
+//   @Override
+//   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//         throws Exception {
+//      
+//      logger.info("로그인 하기 전 . . . . . . . . . .");
+//      HttpSession ses = request.getSession();
+//      
+//      if(ses.getAttribute("loginMember") != null) {    // 이전 로그인 정보가 세션에 남아 있다면
+//            ses.removeAttribute("loginMember");   // 로그인 정보 삭제
+//      }
+//      
+//      return true;   // 이후 인터셉터 또는 컨트롤러에게 제어권을 넘김
+//   }
+//   
+   @Override
+   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+         ModelAndView modelAndView) throws Exception {
 
-			HttpSession ses = request.getSession();
-			ModelMap modelMap = modelAndView.getModelMap();
-			MemberVO vo = (MemberVO)modelMap.get("loginMember");
-			
-			if(vo != null) {
-				logger.info("로그인 성공");
-				logger.info("로그인 유저 : " + vo.toString());
-				
-				ses.setAttribute("loginMember", vo);
-				
-				// 쿠키 처리
-				if(request.getParameter("member_cookie") != null) { // 자동 로그인이 체크 되었을 때
-					System.out.println(request.getParameter("member_cookie"));
-					logger.info("쿠키 처리 ....");
-					
-					Cookie loginCook = new Cookie("ssid", ses.getId());  // ssid 라는 이름으로 세션 ID를 남긴다..(실제 아이디나 비밀번호는 안됨!!)
-					loginCook.setPath("/");
-					loginCook.setMaxAge(60 * 60 * 24 * 7); // 일주일 동안 
-					response.addCookie(loginCook);
-				}
-		
-				String dest = (String)ses.getAttribute("dest");
-				response.sendRedirect((dest != null)? dest : "/");
-			}else {
-				response.sendRedirect("/user/login/yet");
-			}
-			
-			
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+         HttpSession ses = request.getSession();
+         ModelMap modelMap = modelAndView.getModelMap();
+         MemberVO vo = (MemberVO)modelMap.get("loginMember");
+         
+         if(vo != null) {
+            logger.info("로그인 성공");
+            logger.info("로그인 유저 : " + vo.toString());
+            
+            ses.setAttribute("loginMember", vo);
+            
+            // 쿠키 처리
+            if(request.getParameter("member_cookie") != null) { // 자동 로그인이 체크 되었을 때
+               System.out.println(request.getParameter("member_cookie"));
+               logger.info("쿠키 처리 ....");
+               
+               Cookie loginCook = new Cookie("ssid", ses.getId());  // ssid 라는 이름으로 세션 ID를 남긴다..(실제 아이디나 비밀번호는 안됨!!)
+               loginCook.setPath("/");
+               loginCook.setMaxAge(60 * 60 * 24 * 7); // 일주일 동안 
+               response.addCookie(loginCook);
+            }
+      
+            String dest = (String)ses.getAttribute("dest");
+            response.sendRedirect((dest != null)? dest : "/user/login/");
+         } else {
+            String dest = (String)ses.getAttribute("dest");
+            response.sendRedirect((dest == null)? dest : "/user/login/yet");
+         }
+         
+         
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
