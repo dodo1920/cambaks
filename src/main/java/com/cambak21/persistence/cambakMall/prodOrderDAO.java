33273ca34f15defb7df9b4ad53f9a66bd5e7d2db
buggo333 <millal5411@gmail.com>
2021-03-29 16:52:47 +0900

    김대기 / cambak order dao interface, class 생성

---
+++ src/main/java/com/cambak21/persistence/cambakMall/prodOrderDAO.java
@@ -0,0 +1,30 @@
+package com.cambak21.persistence.cambakMall;
+
+import java.util.List;
+
+public interface prodOrderDAO {
+	// 김대기 DAO -->
+		public List<String> selectDestNickName(String member_id) throws Exception;
+//	  	<!-- 장원영 DAO -->
+	  	
+	  	
+//	  	<!-- 김도연 DAO -->
+	  	
+	  	
+//	  	<!-- 김정민 DAO -->
+	  	
+	  	
+//	  	<!-- 김태훈 DAO -->
+	  	
+	  	
+//	  	<!-- 백승권 DAO -->
+	  	
+	  	
+//	  	<!-- 서효원 DAO -->
+	  	
+	  	
+//	  	<!-- 박종진 DAO -->
+	  	
+	  	
+//	  	<!-- 이영광 DAO -->
+}
---
+++ src/main/java/com/cambak21/service/cambakMall/prodOrderService.java
@@ -0,0 +1,5 @@
+package com.cambak21.service.cambakMall;
+
+public interface prodOrderService {
+
+}
