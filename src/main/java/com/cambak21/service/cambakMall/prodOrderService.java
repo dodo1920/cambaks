15361befac4d5fd26612686c23e3f3e378d33ce5
buggo333 <millal5411@gmail.com>
2021-03-29 16:57:13 +0900

    김대기 /  prodOrderService, impl 파일 생성

--- src/main/java/com/cambak21/service/cambakMall/prodOrderService.java
+++ src/main/java/com/cambak21/service/cambakMall/prodOrderService.java
@@ -1,5 +1,32 @@
 package com.cambak21.service.cambakMall;
 
-public interface prodOrderService {
+import java.util.List;
 
+public interface prodOrderService {
+	
+	
+	// 김대기 DAO -->
+	public List<String> selectDestNickName(String member_id) throws Exception;
+//  	<!-- 장원영 DAO -->
+  	
+  	
+//  	<!-- 김도연 DAO -->
+  	
+  	
+//  	<!-- 김정민 DAO -->
+  	
+  	
+//  	<!-- 김태훈 DAO -->
+  	
+  	
+//  	<!-- 백승권 DAO -->
+  	
+  	
+//  	<!-- 서효원 DAO -->
+  	
+  	
+//  	<!-- 박종진 DAO -->
+  	
+  	
+//  	<!-- 이영광 DAO -->
 }
---
+++ src/main/java/com/cambak21/service/cambakMall/prodOrderServiceImpl.java
@@ -0,0 +1,46 @@
+package com.cambak21.service.cambakMall;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.cambak21.persistence.cambakMall.prodOrderDAO;
+
+@Service
+public class prodOrderServiceImpl implements prodOrderService {
+	@Inject
+	private prodOrderDAO dao;
+	
+	// 김대기 DAO -->
+	
+	//주소록 닉네임들을 가져옴
+	@Override
+	public List<String> selectDestNickName(String member_id) throws Exception {
+		
+		return dao.selectDestNickName(member_id);
+	}
+//		  	<!-- 장원영 DAO -->
+		  	
+		  	
+//		  	<!-- 김도연 DAO -->
+		  	
+		  	
+//		  	<!-- 김정민 DAO -->
+		  	
+		  	
+//		  	<!-- 김태훈 DAO -->
+		  	
+		  	
+//		  	<!-- 백승권 DAO -->
+		  	
+		  	
+//		  	<!-- 서효원 DAO -->
+		  	
+		  	
+//		  	<!-- 박종진 DAO -->
+		  	
+		  	
+//		  	<!-- 이영광 DAO -->
+}
