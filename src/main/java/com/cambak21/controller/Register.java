3fb988bfbb8375ee937c43b2b2919ffd6a68283e
webSHW <winspow@gmail.com>
2021-04-09 19:56:07 +0900

    서효원 / 회원가입 이메일 본인 인증 작업 중

--- src/main/java/com/cambak21/controller/Register.java
+++ src/main/java/com/cambak21/controller/Register.java
@@ -1,14 +1,22 @@
 package com.cambak21.controller;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,12 +32,19 @@ public class Register {
 	@Inject
 	private MemberService service;
 	private static final Logger logger = LoggerFactory.getLogger(Register.class);
-  
+
+	@Autowired
+	private JavaMailSenderImpl mailSender;
+	
    
    @RequestMapping(value="register", method = RequestMethod.GET)
-   public String join() {
-      logger.info("회원가입   작업중 . . . . .");
+   public String register() {
       return "cambakMain/user/registetConfirmation";
+   }
+   
+   @RequestMapping(value="join", method = RequestMethod.GET)
+   public String join() {
+      return "cambakMain/user/join";
    }
    
    @RequestMapping(value="register/checkId", method = RequestMethod.POST)
@@ -63,31 +78,51 @@ public class Register {
             	entity = new ResponseEntity<String>("fail", HttpStatus.OK);
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<String>("error", HttpStatus.OK);
+        	e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
         return entity;
    }
    
    @RequestMapping(value="register/sendRegisterEmail", method = RequestMethod.POST)
-   public ResponseEntity<String> sendRegisterEmail(@RequestParam("recaptcha") String recaptcha, HttpServletRequest request) {
+   public ResponseEntity<String> sendRegisterEmail(@RequestParam("userEmail") String userEmail, HttpServletRequest request) throws Exception {
 	   // 유저 recapcha 체크하기
-	   ResponseEntity<String> entity = null;
 	   
-	   GoogleCapcha.setSecretKey("6LcPSqIaAAAAAJL1k-pAZ1KuLXHzGCDG_aB80L8s");
-        //0 = 성공, 1 = 실패, -1 = 오류
-        try {
-            if(GoogleCapcha.verify(recaptcha)) {
-            	entity = new ResponseEntity<String>("success", HttpStatus.OK);
-            } else {
-            	entity = new ResponseEntity<String>("fail", HttpStatus.OK);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<String>("error", HttpStatus.OK);
-        }
+	   ResponseEntity<String> entity = null;
+	   String uuid = UUID.randomUUID().toString();
+	   System.out.println(uuid);
+	   
 
+		if (service.checkRegisterEmail(userEmail)) { // 작성한 이메일 사용 가능
+			   entity = new ResponseEntity<String>("possibility", HttpStatus.OK);
+			   
+				final MimeMessagePreparator preparator = new MimeMessagePreparator() {
+					
+					@Override
+					public void prepare(MimeMessage mimeMessage) throws Exception {
+						String subject = "<Cambak21>에서 보낸 이메일 인증번호 입니다"; // 메일 제목
+						String message = "회원님, <br />"; // 메일 본문
+						message += "회원가입 하려면 ";
+						message += "http://localhost:8081/user/join?user=" + uuid + "&email=" + userEmail;
+						message += " 로 들어오셈";
+						
+						final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+						helper.setFrom("goot6 <goot6610@gmail.com>");
+						helper.setTo(userEmail);
+						helper.setSubject(subject);
+						helper.setText(message, true);
+					}
+				};
+				
+				mailSender.send(preparator); // 메일 발송
+				HttpSession ses = request.getSession();
+				ses.setAttribute("registerUUID", uuid);
+				
+		   } else { // 중복되는 이메일이 있음
+			   entity = new ResponseEntity<String>("impossibility", HttpStatus.OK);
+		   }
+		
         return entity;
    }
    
