5c320fe3ebd9b8501a4e88fb10e2d3ca5c7b3c22
ParkJongJin <13j13@naver.com>
2021-04-26 10:55:07 +0900

    종진 / 병합 후 에러 해결

--- src/main/java/com/cambak21/controller/cambakAdmin/AdminController.java
+++ src/main/java/com/cambak21/controller/cambakAdmin/AdminController.java
@@ -1,8 +1,11 @@
 package com.cambak21.controller.cambakAdmin;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
+import javax.json.JsonObject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,84 +13,132 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cambak21.controller.HomeController;
 import com.cambak21.domain.RevenueVO;
 import com.cambak21.service.cambakAdmin.adminService;
+import com.cambak21.util.PagingCriteria;
+import com.mysql.cj.xdevapi.JsonArray;
+import com.mysql.cj.xdevapi.JsonValue;
+
+import twitter4j.JSONArray;
+import twitter4j.JSONObject;
 
 
 @Controller
 @RequestMapping(value = "/admin/*")
 public class AdminController {
-	
-	@Inject
-	private adminService service;
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String adminIndex() {
-		logger.info("adminIndex호출");
-		
-		return "/admin/index";
-	}
-	
+   
+   @Inject
+   private adminService service;
+   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+   
+   @RequestMapping(value = "/index", method = RequestMethod.GET)
+   public String adminIndex() {
+      logger.info("adminIndex호출");
+      
+      return "/admin/index";
+   }
+   
 
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 대기@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-	@RequestMapping(value = "/revenue", method = RequestMethod.GET)
-	public String revenue() throws Exception{
-		
-		return "/admin/revenue";
-	}
-	@RequestMapping(value = "/revenue/perDate", method = RequestMethod.GET)
-	public ResponseEntity<List<RevenueVO>> revenueIndex(Model model) {
-//		model.addAttribute("revenue", service.getPerDayRevenue());
-		
-		ResponseEntity<List<RevenueVO>> entity = null;
-		
-		try {
-			entity = new ResponseEntity<List<RevenueVO>>(service.getPerDayRevenue(), HttpStatus.OK);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			entity = new ResponseEntity<List<RevenueVO>>(HttpStatus.BAD_REQUEST);
-		}
-		System.out.println(entity);
-		return entity;
-	}
-	
-	
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 대기@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   @RequestMapping(value = "/revenue", method = RequestMethod.GET)
+   public String revenue() throws Exception{
+      
+      return "/admin/revenue";
+   }
+   @RequestMapping(value = "/revenue/perDate", method = RequestMethod.GET)
+   public ResponseEntity<List<RevenueVO>> revenueIndex(@RequestParam("dateVal") int dateVal) {
+//      model.addAttribute("revenue", service.getPerDayRevenue());
+      System.out.println(dateVal);
+      ResponseEntity<List<RevenueVO>> entity = null;
+      
+      try {
+         entity = new ResponseEntity<List<RevenueVO>>(service.getPerDayRevenue(dateVal), HttpStatus.OK);
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+         entity = new ResponseEntity<List<RevenueVO>>(HttpStatus.BAD_REQUEST);
+      }
+      System.out.println(entity);
+      return entity;
+   }
+    
+   @RequestMapping(value = "revenue/selectDate", method = RequestMethod.GET)
+   public ResponseEntity<List<RevenueVO>> selectDate(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, Model model) {
+      
+      
+      
+      ResponseEntity<List<RevenueVO>> entity = null;
+      try {
+         entity = new ResponseEntity<List<RevenueVO>>(service.selectDate(startDate, endDate), HttpStatus.OK);
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+         entity = new ResponseEntity<List<RevenueVO>>(HttpStatus.BAD_REQUEST);
+      }
+      
+      return entity;
+      
+   }
+   @RequestMapping(value = "/revenueWeekly", method = RequestMethod.GET)
+   public String revenueWeekly() throws Exception{
+      
+      return "/admin/revenueWeekly";
+   }
+   @RequestMapping(value = "/revenueMonthly", method = RequestMethod.GET)
+   public String revenueMonthly() throws Exception{
+      
+      return "/admin/revenueMonthly";
+   }
+   
+   
+   
 
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 도연@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 도연@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 정민@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 정민@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 승권@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 승권@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   @GetMapping("/prodRegister")
+   public String productInsert() {
+      logger.info("adminIndex호출");
+      
+      return "/admin/productRegister";
+   }
 
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 종진@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 종진@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   
+   
+   @RequestMapping(value = "/board_admin", method = RequestMethod.GET)
+   public String board_admin_option() throws Exception{
+      
+      return "/admin/board_admin";
+   }
+   
+   
+   
+   
 
-	
-	
-	@RequestMapping(value = "/board_admin", method = RequestMethod.GET)
-	public String board_admin_option() throws Exception{
-		
-		return "/admin/board_admin";
-	}
-	
-	
-	
-	
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 효원@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   
+   @RequestMapping(value="/orderManagement")
+   public String orderManagement(PagingCriteria cri, Model model) throws Exception{
+      model.addAttribute("order", service.readOrderList(cri));
+      return "/admin/adminOrderManagement";
+   }
+   
+   
+   //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 원영@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 효원@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 원영@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-	
+   
 }
-
