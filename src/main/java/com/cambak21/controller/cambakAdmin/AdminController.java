23510407a68b312cf488e4818576d7f423435450
william8856 <william8457@gmail.com>
2021-05-03 13:16:23 +0900

    원영 / QA관리자페이지 작업중

--- src/main/java/com/cambak21/controller/cambakAdmin/AdminController.java
+++ src/main/java/com/cambak21/controller/cambakAdmin/AdminController.java
@@ -752,5 +752,75 @@ public class AdminController {
    
    
    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 원영@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+   
+   @RequestMapping(value = "/QA", method = RequestMethod.GET)
+   public String QA_option() throws Exception{
+      
+      return "/admin/QA_admin";
+   }
+   
+   @RequestMapping(value = "/QA/ajax/{goStartDate}/{goEndDate}/{board_category}/{searchselectedCategory}/{searchboardType}/{searchTxtValue}/{page}", method = RequestMethod.GET)
+   public ResponseEntity<Map<String, Object>> getNewProduct(@PathVariable("goStartDate") String goStartDate, @PathVariable("goEndDate") String goEndDate, @PathVariable("board_category") String board_category, @PathVariable("searchselectedCategory") String searchselectedCategory, @PathVariable("searchboardType") String searchboardType, @PathVariable("searchTxtValue") String searchTxtValue, @PathVariable("page") int page, Locale locale, Model model, HttpServletRequest request, HttpServletResponse response) throws ParseException {
+	   ResponseEntity<Map<String, Object>> entity = null;
+	
+	   Map<String, Object> para = new HashMap<String, Object>();
+	   List<AdminBoardDTO> Boardlst = new ArrayList<AdminBoardDTO>();
+	   List<AdminReplyBoardDTO> replyBoardlst = new ArrayList<AdminReplyBoardDTO>();
+	   
+	   PagingCriteria pc = new PagingCriteria();
+	   pc.setPage(page);
+	   PagingParam pp = new PagingParam();
+	   pp.setCri(pc);
+	   
+	   BoardAdminSearchCriteria BAcri1 = new BoardAdminSearchCriteria(goStartDate, goEndDate, board_category);
+	   BoardAdminSearchCriteria BAcri2 = new BoardAdminSearchCriteria(goStartDate, goEndDate, board_category, searchboardType, searchTxtValue);
+	   
+	   try {
+			if(searchTxtValue.equals("none")) {
+				
+			
+			    if(searchselectedCategory.equals("board")) {
+			    	Boardlst = service.goGetBoard_admin(BAcri1, pc);
+			    	pp.setTotalCount(service.getBoard_adminCnt(BAcri1));
+			    	para.put("Boardlst", Boardlst);
+			    }
+				
+			    if(searchselectedCategory.equals("reply")) {
+			    	replyBoardlst = service.goGetreply_admin(BAcri1, pc);
+			    	pp.setTotalCount(service.getReply_adminCnt(BAcri1));
+			    	para.put("replyBoardlst", replyBoardlst);
+			    }
+			
+			}else {
+			
+				 if(searchselectedCategory.equals("board")) {
+				    	Boardlst = service.searchGetBoard_admin(BAcri2,pc);
+				    	pp.setTotalCount(service.getsearchBoard_adminCnt(BAcri2));
+				    	para.put("Boardlst", Boardlst);	
+				    }
+					
+				    if(searchselectedCategory.equals("reply")) {
+				    	replyBoardlst = service.searchGetreply_admin(BAcri2, pc);
+				    	pp.setTotalCount(service.getsearchReply_adminCnt(BAcri2));
+				    	para.put("replyBoardlst", replyBoardlst);	
+				    }
+		
+			    
+			  
+			}
+			 para.put("todayTotCnt", service.getTodayTotCnt());
+			 para.put("todayreplyTotCnt", service.getTodayreplyTotCnt());
+			 para.put("pagingParam", pp);
+			 entity = new ResponseEntity<Map<String, Object>>(para, HttpStatus.OK);
+			 
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	   
+	   	
 
+			return entity;
+	   }
+   
 }
